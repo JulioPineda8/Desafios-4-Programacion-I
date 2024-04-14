f34f962ef788b48b2b45ac1dcfedbb1928d8b25d
JulioPineda <jpinedad5@miumg.edu.gt>
2024-04-14 17:19:06 -0600

    primer commit

---
+++ Deafio2/Deafio2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public interface INotificable
+{
+    void Notificar();
+}
+
+public class NotificacionEmail : INotificable
+{
+    private string DireccionEmail;
+
+    public NotificacionEmail(string email)
+    {
+        this.DireccionEmail = email;
+    }
+
+    public void Notificar()
+    {
+        Console.WriteLine("SE ENVIO UNA NOTIFICACION POR CORREO ELECTRONICO A: " + DireccionEmail);
+
+    }
+}
+public class NotificacionSMS : INotificable
+{
+    private string NumeroTelefono;
+
+    public NotificacionSMS(string numeroTelefono)
+    {
+        this.NumeroTelefono = numeroTelefono;
+    }
+
+    public void Notificar()
+    {
+        Console.WriteLine("SE HA ENVIADO UN SMS AL NUMERO: " + NumeroTelefono);
+
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        NotificacionSMS notificacionSMS = new NotificacionSMS("4363-0548");
+        notificacionSMS.Notificar();
+
+        NotificacionEmail notificacionEmail = new NotificacionEmail("jpinedad5@miumg.edu.gt");
+        notificacionEmail.Notificar();
+
+    }
+}
---
+++ Desafio1/Desafio1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+
+public class Calculadora
+{
+  
+    public string Marca { get; set; }
+    public string Serie { get; set; }
+
+    
+    public Calculadora(string marca, string serie)
+    {
+        Marca = marca;
+        Serie = serie;
+    }
+
+    
+    public double Sumar(double a, double b)
+    {
+        return a + b;
+    }
+
+    public double Restar(double a, double b)
+    {
+        return a - b;
+    }
+
+    public double Multiplicar(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double Dividir(double a, double b)
+    {
+        if (b != 0)
+        {
+            return a / b;
+        }
+        else
+        {
+            Console.WriteLine("Error: División por cero.");
+            return double.NaN;
+        }
+    }
+}
+
+
+public class CalculadoraCientifica : Calculadora
+{
+    
+    public CalculadoraCientifica(string marca, string serie) : base(marca, serie)
+    {
+    }
+
+    
+    public double Potencia(double baseNum, double exponente)
+    {
+        return Math.Pow(baseNum, exponente);
+    }
+
+    public double Raiz(double numero)
+    {
+        return Math.Sqrt(numero);
+    }
+
+    public double Modulo(double a, double b)
+    {
+        return a % b;
+    }
+
+    public double Logaritmo(double numero, double baseLog)
+    {
+        return Math.Log(numero, baseLog);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var calculadoraBasica = new Calculadora("Casio", "1234");
+        var calculadoraCientifica = new CalculadoraCientifica("HP", "5678");
+        Console.WriteLine("casio, 1234");
+        Console.WriteLine("HP, 5678");
+
+        
+        Console.WriteLine($"Suma: {calculadoraBasica.Sumar(5, 3)}");
+        Console.WriteLine($"Resta: {calculadoraBasica.Restar(5, 3)}");
+        Console.WriteLine($"Multiplicación: {calculadoraBasica.Multiplicar(5, 3)}");
+        Console.WriteLine($"División: {calculadoraBasica.Dividir(5, 3)}");
+
+        
+        Console.WriteLine($"Potencia: {calculadoraCientifica.Potencia(2, 3)}");
+        Console.WriteLine($"Raíz cuadrada: {calculadoraCientifica.Raiz(16)}");
+        Console.WriteLine($"Módulo: {calculadoraCientifica.Modulo(5, 3)}");
+        Console.WriteLine($"Logaritmo: {calculadoraCientifica.Logaritmo(100, 10)}");
+    }
+}
